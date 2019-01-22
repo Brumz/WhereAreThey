07202e359d99badc4a50ee4e4d2326e72867cdfd
Brumz <bmanrumsey24@gmail.com>
2019-01-22 15:43:23 -0700

    build lake and added script for gem

---
+++ Assets/Scripts/GemRotate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemRotate : MonoBehaviour {
+
+    public int RotateSpeed;
+    public AudioSource CollectSound;
+    public GameObject Gem;
+
+	void Update () {
+        transform.Rotate(0, RotateSpeed, 0, Space.World);
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        CollectSound.Play();
+        Gem.SetActive(false);
+    }
+}
