dc191f3fdba94de7ad8373c5a77bea17f8e88cf4
Brumz <bmanrumsey24@gmail.com>
2019-01-25 10:26:33 -0700

    Added axe, axe sound, created script for axe swing and sound, created layers so the axe wont clip through anything

---
+++ Assets/Scripts/AxeSwing.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AxeSwing : MonoBehaviour {
+
+    public GameObject TheAxe;
+    public bool isSwinging;
+    public AudioSource SwingSound; 
+
+	void Update () {
+		if(Input.GetButtonDown("Fire1") && isSwinging == false)
+        {
+            isSwinging = true;
+            StartCoroutine(SwingTheAxe());
+        }
+    }
+    IEnumerator SwingTheAxe()
+    {
+        SwingSound.Play();
+        TheAxe.GetComponent<Animation>().Play("SwingAxe");
+        yield return new WaitForSeconds(0.55f);
+        isSwinging = false;
+    }
+}
