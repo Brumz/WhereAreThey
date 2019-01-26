b942aec77eefc37d9684783e3a7ca3f8822485da
Brumz <bmanrumsey24@gmail.com>
2019-01-26 15:51:20 -0700

    Added cabins and bushes, created script for cabin door

---
+++ Assets/Scripts/VillageScripts/DoorOpenHouse.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpenHouse : MonoBehaviour {
+
+    public GameObject theDoor;
+    public AudioSource creakSound;
+
+    void OnTriggerEnter(Collider other)
+    {
+        creakSound.Play();
+        theDoor.GetComponent<Animator>().enabled = true;
+        theDoor.GetComponent<Animator>().Play("OpenDoorVillage");
+        this.GetComponent<BoxCollider>().enabled = false;
+    }
+
+}
